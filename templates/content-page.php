001df4c5be5f24ce9f4f00281f365f6dc3939168
dwpierce <dwpierce@wsu.edu>
2016-03-14 17:03:34 -0700

    Created content page template in templates folder

---
+++ templates/content-page.php
@@ -0,0 +1,9 @@
+<article>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+</article>
