e06a1cd5dde50e09c21b4b81f44d9a1b9bd6b599
dwpierce <dwpierce@wsu.edu>
2016-03-14 17:03:47 -0700

    Delete content-page.php

--- content-page.php
+++
@@ -1,9 +0,0 @@
-<article>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header>
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div>
-</article>
